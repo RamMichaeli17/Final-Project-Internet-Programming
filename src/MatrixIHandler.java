4411efa4da6f568a570e0999ed39f28efda45cfd
sapir daga <sapirddd001@gmail.com>
2021-10-05 20:45:50 +0300

    adding client & server warp accept in separate thread

---
+++ src/MatrixIHandler.java
@@ -0,0 +1,183 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * This class handles server.Matrix-related tasks
+ */
+public class MatrixIHandler implements IHandler {
+    private Matrix matrix;
+    private Index startIndex, endIndex;
+    private volatile boolean doWork = true;
+
+
+    private void resetMembers() {
+        this.matrix = null;
+        this.startIndex = null;
+        this.endIndex = null;
+        this.doWork = true;
+    }
+    public List<HashSet<Index>> findSCCs(int[][] source)
+    {
+        HashSet<HashSet<Index>> listOFHashsets;
+        List<Index> listOne;
+        Matrix sourceMatrix = new Matrix(source);
+        sourceMatrix.printMatrix();
+
+        //parallelDFSTraverse need to get traversable<T> , list<HashSet<Index>>> :
+        TraversableMatrix myTraversableM = new TraversableMatrix(sourceMatrix);
+        listOne = sourceMatrix.findAllOnes();
+        System.out.println(listOne);
+
+        //set the first index
+        myTraversableM.setStartIndex(myTraversableM.getStartIndex());
+        ThreadLocalDFSVisit<Index> algo = new ThreadLocalDFSVisit<>();
+
+        //call to parallelDFSTraverse method
+        listOFHashsets = algo.parallelDFSTraverse(myTraversableM, listOne); //the problematic line
+        List<HashSet<Index>> list = listOFHashsets.stream().sorted(Comparator.comparingInt(HashSet::size))
+                .collect(Collectors.toList());
+        return list;
+
+    }
+
+    @Override
+    public void handle(InputStream fromClient, OutputStream toClient) throws IOException, ClassNotFoundException {
+        /*
+        Send data as bytes.
+        Read data as bytes then transform to meaningful data
+        ObjectInputStream and ObjectOutputStream can read and write both primitives and objects
+         */
+        ObjectInputStream objectInputStream = new ObjectInputStream(fromClient);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(toClient);
+
+        this.resetMembers();
+
+        boolean doWork = true;
+        // handle client's tasks
+        while(doWork){
+          //We use switch-case in order to get commands from client (each task has case).
+
+            switch (objectInputStream.readObject().toString()){
+                //TODO: each case for each task
+                /*case "matrix":{
+                    // client will now send a 2d array. handler will create a matrix object
+                    int[][] tempArray = (int[][])objectInputStream.readObject();
+                    System.out.println("Server: Got 2d array");
+                    this.matrix = new Matrix(tempArray);
+                    this.matrix.printMatrix();
+                    break;
+                }
+
+                case "getNeighbors":{
+                    // handler will receive an index, then compute its neighbors
+                    Index tempIndex = (Index)objectInputStream.readObject();
+                    if(this.matrix!=null){
+                        List<Index> neighbors = new ArrayList<>(this.matrix.getNeighbors(tempIndex));
+                        System.out.println("Server: neighbors of "+ tempIndex + ":  " + neighbors);
+                        // send to socket's outputstream
+                        objectOutputStream.writeObject(neighbors);
+                    }
+                    break;
+                }
+
+                 case "getReachables":{
+                    // handler will receive an index, then compute its neighbors
+                    Index tempIndex = (Index)objectInputStream.readObject();
+                    if(this.matrix!=null){
+                        List<Index> reachables = new ArrayList<>(this.matrix.getReachables(tempIndex));
+                        System.out.println("Server: neighbors of "+ tempIndex + ":  " + reachables);
+                        // send to socket's outputstream
+                        objectOutputStream.writeObject(reachables);
+                    }
+                }
+
+                //cases added
+                case "start index":{
+                    this.startIndex = (Index)objectInputStream.readObject();
+                    break;
+                }
+
+                case "end index":{
+                    this.endIndex = (Index)objectInputStream.readObject();
+                    break;
+                }*/
+
+                case "1":{ //dfs
+                    //we convert 2D array to primitive matrix
+                    int[][] primitiveMatrix = (int[][])objectInputStream.readObject();
+                    System.out.println("Server: Got 2d array from client");
+                    List<HashSet<Index>> listOFHashsets;
+                    //calling method will find the SCCs
+                    listOFHashsets=findSCCs(primitiveMatrix);
+                    //transfers to client the answer
+                    objectOutputStream.writeObject(listOFHashsets);
+                    System.out.println("from server: Reachable Indecies are - " + listOFHashsets);
+                    System.out.println("Task1 finish");
+                    break;
+                }
+                case "2": { //bfs
+                    int[][] primitiveMatrix = (int[][]) objectInputStream.readObject();
+                    System.out.println("Server: Got 2d array from client");
+                    this.matrix=new Matrix(primitiveMatrix);
+                    matrix.printMatrix();
+                    Index src, dest;
+                    System.out.println("Please enter source index");
+                    src=(Index)objectInputStream.readObject();
+                    System.out.println("From client - source index is :"+ src);
+                    System.out.println("Please enter destination index");
+                    dest=(Index)objectInputStream.readObject();
+                    System.out.println("From client - destination index is :"+ dest);
+                    TraversableMatrix traversable2 = new TraversableMatrix(this.matrix);
+                    traversable2.setStartIndex(src);
+                    traversable2.setEndIndex(dest);
+
+                    System.out.println("From server - find shortest paths:");
+                    System.out.println("start" + traversable2.getOrigin() + "end " + traversable2.getDestination());
+                    //TODO: create an object of BFS class and call it function
+
+                    System.out.println("Task2 finish");
+                    System.out.println("Task2 finish");
+                    break;
+                }
+                case "3":{ //submarines
+                    int[][] primitiveMatrix = (int[][]) objectInputStream.readObject();
+                    System.out.println("Server: Got 2d array from client");
+                    //TODO: pull task 3 ,create an object of it class and call it function
+
+                    System.out.println("Task3 finish");
+                  break;
+                }
+                case "4":{ //the lightest paths with bfs / bellmanFord
+                    int[][] primitiveMatrix = (int[][])objectInputStream.readObject();
+                    Index src, dest;
+                    System.out.println("Server: Got 2d array from client");
+                    this.matrix=new Matrix(primitiveMatrix);
+                    matrix.printMatrix();
+                    System.out.println("Please enter source index");
+                    src=(Index)objectInputStream.readObject();
+                    System.out.println("From client - source index is :"+ src);
+                    System.out.println("Please enter destination index");
+                    dest=(Index)objectInputStream.readObject();
+                    System.out.println("From client - destination index is :"+ dest);
+                    TraversableMatrix traversable4 = new TraversableMatrix(this.matrix);
+                    traversable4.setStartIndex(src);
+                    traversable4.setEndIndex(dest);
+                    ThreadLocalBellmanFord bellmanFord= new ThreadLocalBellmanFord();
+                    LinkedList<List<Index>> minWeightList;
+                    //TODO: solve the problem- the method return an empty list
+                    minWeightList = bellmanFord.findPathsBellmanFord(traversable4, traversable4.getOrigin(), traversable4.getDestination());
+                    System.out.println(minWeightList);
+                    objectOutputStream.writeObject(minWeightList);
+                    System.out.println("Task4 finish");
+                    break;
+                }
+
+                case "stop":{
+                    doWork = false;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ src/TcpServer.java
@@ -0,0 +1,133 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class TcpServer {
+
+    private final int port; // initialize in constructor
+    private volatile boolean stopServer; // volatile - stopServer variable is saved in RAM memory
+    private ThreadPoolExecutor threadPool; // handle each client in a separate thread
+    private IHandler requestHandler; // what is the type of clients' tasks
+
+    private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public TcpServer(int port){
+        this.port = port;
+        // initialize data members (although they are initialized by default), for readability
+        stopServer = false;
+        this.threadPool = null;
+        requestHandler = null;
+
+    }
+
+    public void supportClients(IHandler handler) {
+        this.requestHandler = handler;
+       /*   2 Kinds of sockets
+            Server Socket - a server sockets listens and wait for incoming connections
+            1. server socket binds to specific port number
+            2. server socket listens to incoming connections
+            3. server socket accepts incoming connections if possible
+
+            Operational socket (client socket)
+            Server Socket API:
+             1. create socket
+             2. bind to a specific port number
+             3. listen for incoming connections (a client initiates a tcp connection with server)
+             4. try to accept (if 3-way handshake is successful)
+             5. return operational socket (2 way pipeline)
+        */
+
+        //accept() warp by another thread
+        new Thread(() ->{
+
+           this.threadPool = new ThreadPoolExecutor(3,5,
+                    10, TimeUnit.SECONDS, new LinkedBlockingQueue());
+
+            try {
+                ServerSocket serverSocket = new ServerSocket(this.port); // bind
+                /*
+                listen to incoming connection and accept if possible
+                be advised: accept is a blocking call
+                TODO: wrap in another thread
+                */
+                while(!stopServer){
+
+                    Socket serverClientConnection = serverSocket.accept();   //accept()
+                    // define a task and submit to our threadPool
+
+                    /*server will handle each client in a separate thread
+                       define every client as a Runnable task to execute*/
+
+                    Runnable clientHandling = ()->{
+                        System.out.println("Server: Handling a client");
+                        try {
+                            //right now we have a problem with the line below
+                            requestHandler.handle(serverClientConnection.getInputStream(),
+                                    serverClientConnection.getOutputStream());
+                            // terminate connection with client
+                            // close all streams
+                            serverClientConnection.getInputStream().close();
+                            serverClientConnection.getOutputStream().close();
+                            serverClientConnection.close();
+                        } catch (IOException | ClassNotFoundException ioException) {
+                            ioException.printStackTrace();
+                        }
+                    };
+                    threadPool.execute(clientHandling);
+                }
+                serverSocket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+
+        }).start();
+    }
+
+    /* public void stop(){
+        if(!stopServer){
+            stopServer = true;
+            if(threadPool!=null)
+            if(threadPool!=null)
+                threadPool.shutdown();
+        }
+
+    }*/
+    public void stop(){
+        if(!stopServer){
+            try{
+                //double-checked locking
+                readWriteLock.writeLock().lock();
+                if(!stopServer){
+                    if(threadPool!=null)
+                        threadPool.shutdown();
+                }
+            }catch (SecurityException se){
+                se.printStackTrace();
+            }finally {
+                stopServer = true;
+                readWriteLock.writeLock().unlock();
+                System.out.println("Server shut down successfully");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TcpServer webServer = new TcpServer(8010);
+        webServer.supportClients(new MatrixIHandler());
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Stopping the server");
+        webServer.stop();
+    }
+
+
+
+}
+
