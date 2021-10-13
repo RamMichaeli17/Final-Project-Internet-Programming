6201606c73d70c574fc5c701e1fc1f2e34a4b4c5
ozweiss <oz.weiss@roojoom.com>
2021-10-13 20:40:35 +0300

    tcp server

--- src/TcpServer.java
+++ src/TcpServer.java
@@ -6,6 +6,13 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/*
+        transport layer - multiplexing. send different kinds of data over the communication
+
+        Socket - abstraction for 2-way pipeline of data (of certain kind)
+        the kind is determined by the socket number
+*/
+
 public class TcpServer {
 
     private final int port; // initialize in constructor
@@ -25,24 +32,13 @@ public class TcpServer {
 
     public void supportClients(IHandler handler) {
         this.requestHandler = handler;
-       /*   There are 2 Kinds of sockets:
 
-           (1) Server Socket - a server sockets listens and wait for incoming connections
-            1. server socket binds to specific port number
-            2. server socket listens to incoming connections
-            3. server socket accepts incoming connections if possible
+        /*
+         A server can do many things. Dealing with listening to clients and initial
+         support is done in a separate thread
+         */
 
-            Server Socket API:
-             1. create socket
-             2. bind to a specific port number
-             3. listen for incoming connections (a client initiates a tcp connection with server)
-             4. try to accept (if 3-way handshake is successful)
-             5. return operational socket (2 way pipeline)
 
-           (2) Operational socket (client socket)
-       */
-
-        //warp clientHandling Runnable by another thread -
         new Thread(() ->{
 
            this.threadPool = new ThreadPoolExecutor(3,5,
@@ -56,12 +52,10 @@ public class TcpServer {
                 why?
                 When you call accept(), and there isn't already a client connecting to you,it will return 'Operation Would Block'
                 to tell you that it can't complete accept() without waiting...
-                TODO: wrap in another thread
                 */
                 while(!stopServer){
 
                     Socket serverClientConnection = serverSocket.accept();
-
                     // define a task and submit to our threadPool
 
                     /*server will handle each client in a separate thread
